14c35be3cb68392ef5d3000bc122b19288765711
Tiago Dávila <tiago.davila@mindera.com>
2021-01-19 14:31:10 +0000

    Enhancing main with useful information

--- src/conventions/MainConventions.java
+++ src/conventions/MainConventions.java
@@ -5,6 +5,7 @@ public class MainConventions {
     public static void main(String[] args) {
 
         System.out.println("Primeira aula");
+        System.out.println("Git is fucking important");
         
     }
 }
