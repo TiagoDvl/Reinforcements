cdb97dc9c2dc8729953c5602f09a5ddf7fda6b99
antonio alfama <antonioalfama8@gmail.com>
2021-02-15 17:50:11 +0000

    List exercise improvement

--- src/tools/exercices/supermarket/Customer.java
+++ src/tools/exercices/supermarket/Customer.java
@@ -12,17 +12,20 @@ public class Customer {
     }
 
     public int getDebt() {
-      List<Integer> customerDebt = new ArrayList();
-      customerDebt.add(11);
-      customerDebt.add(22);
-      customerDebt.add(33);
-      customerDebt.add(44);
-
-
-      for (int i = 0; i < customerDebt.size(); i++) {
-          debt = debt + customerDebt.get(i);
-      }
       return debt;
+    }
+
+    public void amountDebt() {
+        List<Integer> customerDebt = new ArrayList();
+        customerDebt.add(11);
+        customerDebt.add(22);
+        customerDebt.add(33);
+        customerDebt.add(44);
+
+
+        for (int i = 0; i < customerDebt.size(); i++) {
+            debt = debt + customerDebt.get(i);
+        }
     }
 
     public void setDebt(int debt) {
--- src/tools/exercices/supermarket/Main.java
+++ src/tools/exercices/supermarket/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Customer customer = new Customer("Tiago");
+        customer.amountDebt();
         Cashier cashier = new Cashier(customer);
         cashier.checkout(PaymentMethod.CARD);
     }
